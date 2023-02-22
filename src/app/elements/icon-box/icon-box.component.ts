cd7aac131126b19fe038d0de1d9b3f19eb561716
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-02-21 20:06:02 -0600

    skeleton template

--- src/app/elements/icon-box/icon-box.component.ts
+++ src/app/elements/icon-box/icon-box.component.ts
@@ -12,17 +12,17 @@ export class IconBoxComponent implements OnInit {
   icon_box : any = [
                   {
                     title:"Address",
-                    description:"8901 Marmora Road Chi Minh City, Vietnam",
+                    description:"2210 Apache Road,<br/>Huntsville, Alabama 35810",
                     icon_class:"la-map-marker",
                   },
                   {
                     title:"Email",
-                    description:'<a href="javascript:void(0);">info@example.com</a><br/><a href="javascript:void(0);">info@example.com</a>',
+                    description:'<a href="javascript:void(0);">jt.aws.cloud@protonmail.com</a><br/><a href="javascript:void(0);">jt.aws.cloud@protonmail.com</a>',
                     icon_class:"la-envelope-open",
                   },
                   {
                     title:"Phone",
-                    description:"(+1) 123 456 7890<br/>(+91) 423 547 8909",
+                    description:"(256) 746 7370",
                     icon_class:"la-phone-volume",
                   },
             ];
